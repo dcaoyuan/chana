1292f8d20a01a8cc89f3a29e10ad6f1125d623cd
Caoyuan Deng <dcaoyuan@gmail.com>
2015-01-16 01:12:40 +0800

    Added union defined testing avsc: Kwafer.avsc

---
+++ src/test/resources/avsc/test-kwafer.sh
@@ -0,0 +1,22 @@
+#/bin/sh
+
+curl --data @Kwafer.avsc 'http://localhost:8080/putschema/kwafer/com.wandoujia.kwaf.schema.Kwafer'
+# OK
+printf "\n"
+
+curl 'http://localhost:8080/kwafer/get/1'
+#
+printf "\n"
+
+curl --data-binary @Kwafer.update 'http://localhost:8080/kwafer/put/1'
+# OK
+printf "\n"
+
+curl 'http://localhost:8080/kwafer/get/1'
+#
+printf "\n"
+
+curl --data '.accountRecords[0].action.type' 'http://localhost:8080/kwafer/select/1'
+# 
+printf "\n"
+
